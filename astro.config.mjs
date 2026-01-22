a4f7b95903ae97d4250228601613f6816395041f
tonyverc <tony.vercruysse@hotmail.com>
2026-01-22 15:41:33 +0100

    compression image pour mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,25 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 4321,
+  },
+  build: {
+    inlineStylesheets: 'auto',
+    assets: '_astro',
+  },
+  vite: {
+    build:{
+      cssCodeSplit: true,
+      minify: 'esbuild',
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
+    },
+  },
   integrations: [tailwind()],
-  site: 'https://tn-web-concept.fr',
+  site: 'https://tnwebconcept.fr',
 });
